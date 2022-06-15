e2234c10bdef69fce44159a8691c024418983a64
Travis Michette <tmichett@redhat.com>
2022-06-15 11:15:35 -0400

    Python PDF Start

--- Container/adocpdf.py
+++ Container/adocpdf.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 #
-#
-# Chris Tusa <ctusa@redhat.com>
 # Travis Michette <tmichett@redhat.com>
 # (C)opyright 2022 : Red Hat, Inc. - see LICENSE
 #
@@ -10,32 +8,15 @@ VERSION = 20220614
 
 import sys
 import platform
-import argparse
-from pathlib import Path
-import shlex
 import subprocess
-import yaml
-
-
-def runcmd(cmdline):
-    """
-    Wrapper to parse a command line and execute with subprocess module
-    :param cmdline:
-    :return:
-    """
-    result = subprocess.run(splitargs(cmdline), capture_output=True, text=True)
-    return result
+import getopt
 
 
 
-def getpodid(image, tag):
-    if TOOLSET == 'podman':
-        cmdline = f"podman inspect --type image --format '{{{{.Id}}}}' {cfg['image']}:{cfg['tag']}"
-    elif TOOLSET == 'docker':
-        cmdline = f"docker inspect --type image --format '{{{{.Id}}}}' {cfg['image']}:{cfg['tag']}"
-    result = runcmd(cmdline)
-    return result.stdout
+     $book_arg"
 
+current_directory = str(Path.cwd())
+container = "quay.io/tmichett/asciidocpdf:latest"
+pdf_theme = "-a pdf-theme=/opt/asciidoc/themes/tm-gls-redhat-theme.yml"
 
- cmdline = f"docker }"
-    result = runcmd(cmdline)
+subprocess.run(["podman", "run -it", "--name adocpdf","--rm", "-v {current_directory}:/tmp/coursebook"], {container}, {pdf_theme})
