00102b8c2dc57bb3559a7b5f8fbcaad014cc520c
Travis Michette <tmichett@redhat.com>
2022-08-30 13:02:49 -0400

    Fixed with Symbolic Links and have Setup Script to setup templates for me

---
+++ Setup_Document_Template.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "**********************************************"
+echo "************ Create Chapters *****************"
+echo "**********************************************"
+
+echo "How many Chapters in this Book"
+read CHAPTERS
+
+for ((i=2;i<=$CHAPTERS;i++))
+do
+  cp ChapterTMP.adoc Chapter$i
+done
+
+echo "**********************************************"
+echo "***** Create Chapters Sections/Folders *******"
+echo "**********************************************"
+
+cd Chapters
+for ((i=2;i<=$CHAPTERS;i++))
+do
+  cp -avR CH_TMP CH$i
+done
