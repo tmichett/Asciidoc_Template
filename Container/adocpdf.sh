fdb3d50b34444b1f408838c38f8d6fe91dd52a39
Travis Michette <tmichett@redhat.com>
2022-12-22 14:18:16 -0500

    updated launch script and tested on Mac

--- Container/adocpdf.sh
+++ Container/adocpdf.sh
@@ -14,4 +14,4 @@ echo "Command being run is: podman run --name adocpdf --rm -v $current_directory
 #podman run --name adocpdf --rm -v $current_directory:/tmp/coursebook:Z $container $pdf_theme $1
 
 
-podman run --name adocpdf --rm -v $current_directory:/tmp/coursebook:Z  -e "pdf_theme=${pdf_theme}" -e "book_name=${1}" adocpdf-test
+podman run --name adocpdf --rm -v $current_directory:/tmp/coursebook:Z  -e "pdf_theme=${pdf_theme}" -e "book_name=${1}" $container
