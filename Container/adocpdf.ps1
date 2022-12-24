7fa56f680eea97c27b49bcb10ddfabc05eab4cc1
Travis Michette <tmichett@gmail.com>
2022-12-23 23:22:32 -0500

    updated Powershell script

--- Container/adocpdf.ps1
+++ Container/adocpdf.ps1
@@ -4,4 +4,4 @@ $container = "quay.io/tmichett/asciidocpdf:latest"
 $current_directory = (get-location).path
 $pdf_theme="-a pdf-theme=/opt/asciidoc/themes/tm-gls-redhat-theme.yml"
 Write-Output $current_directory
-Invoke-Expression "docker run -it --name adocpdf --rm -v $current_directory`:/tmp/coursebook $container $pdf_theme $book_arg"
+Invoke-Expression "docker run -it --name adocpdf --rm -e 'pdf_theme=$pdf_theme' -e 'book_name=$book_arg'-v $current_directory`:/tmp/coursebook $container"
